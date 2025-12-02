08719bdf95ed96ae12ba9f5706f5b8a19687f572
leonardo fiuza <l.fiuza@ua.pt>
2025-12-02 19:42:17 +0000

    node vertion && page filters root fix

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("meal/:id", "routes/meal.tsx"),
-    route("Filters", "routes/Filters.tsx"),
+    route("Filters", "routes/filters.tsx"),
 ] satisfies RouteConfig;
